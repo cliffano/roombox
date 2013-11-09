645999dacaa2b8781de76133b1a7f4512bbd9308
Cliffano Subagio <cliffano@gmail.com>
2013-11-10 00:29:38 +1100

    Fix directory files reading.

--- test/playlist.js
+++ test/playlist.js
@@ -21,7 +21,7 @@ buster.testCase('playlist - load', {
       'z6G2|[E2C,2]C,2[zC,]E[GC,]A|[D2G,2]G,2[z2G,2][DG,]E|[F2G,2][c2G,2]G,c[BG,]G|[AF,]G/F/[E2C,2]C,2[zC,]G|[AF,]AF,A[d/F,/]cB/F,/c/A|[G2C,2]C,2[CC,]D[EC,]A|'
     ].join('\n'));
     this.mockFs.expects('readdirSync').withExactArgs('somedir').returns(['somesong.abc']);
-    this.mockFs.expects('readFileSync').withExactArgs('somesong.abc').returns(data);
+    this.mockFs.expects('readFileSync').withExactArgs('somedir/somesong.abc').returns(data);
     var playlist = new Playlist();
     playlist.load('somedir');
     assert.equals(playlist.songs.length, 1);
@@ -37,7 +37,7 @@ buster.testCase('playlist - load', {
   'should log error message when converter does not exist': function () {
     this.mockConsole.expects('error').withExactArgs('Unable to convert %s', 'somesong.xyz');
     this.mockFs.expects('readdirSync').withExactArgs('somedir').returns(['somesong.xyz']);
-    this.mockFs.expects('readFileSync').withExactArgs('somesong.xyz').returns('somecontent');
+    this.mockFs.expects('readFileSync').withExactArgs('somedir/somesong.xyz').returns('somecontent');
     var playlist = new Playlist();
     playlist.load('somedir');
     assert.equals(playlist.songs.length, 0);
@@ -48,14 +48,15 @@ buster.testCase('playlist - getSong', {
   setUp: function () {
     this.playlist = new Playlist();
     this.playlist.songs = [
-      { title: 'sometitle', by: 'someartist', notes: [60, 64] }
+      { title: 'sometitle', by: 'someartist', notes: [[60, 64], [30, 64]] }
     ];
   },
   'should return song when song exists': function () {
     var song = this.playlist.getSong(1);
     assert.equals(song.title, 'sometitle');
     assert.equals(song.by, 'someartist');
-    assert.equals(song.notes, [60, 64]);
+    assert.equals(song.notes[0], [60, 64]);
+    assert.equals(song.notes[song.notes.length - 1], [30, 64]);
   },
   'should return undefined when song does not exist': function () {
     var song = this.playlist.getSong(2000);
