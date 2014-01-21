759174fc997c8efbe77eb1897a0fa056f2108861
Cliffano Subagio <cliffano@gmail.com>
2014-01-22 00:06:05 +1100

    Add more tests, boost coverage.

---
+++ test/cli.js
@@ -0,0 +1,43 @@
+var bag = require('bagofcli'),
+  buster = require('buster-node'),
+  cli = require('../lib/cli'),
+  prompt = require('prompt'),
+  referee = require('referee'),
+  Roombox = require('../lib/roombox'),
+  assert = referee.assert;
+
+buster.testCase('cli - exec', {
+  'should contain commands with actions': function (done) {
+    var mockCommand = function (base, actions) {
+      assert.defined(base);
+      assert.defined(actions.commands.start.action);
+      done();
+    };
+    this.stub(bag, 'command', mockCommand);
+    cli.exec();
+  }
+});
+
+buster.testCase('cli - start', {
+  setUp: function () {
+    this.mockProcess = this.mock(process);
+    this.mockPrompt = this.mock(prompt);
+  },
+  'should prompt for track number ': function () {
+    this.stub(bag, 'command', function (base, actions) {
+      actions.commands.start.action({ baudRate: 57600, path: '/dev/tty' });
+    });
+    this.mockPrompt.expects('start').twice().withExactArgs();
+    this.mockPrompt.expects('get').once().withArgs(['Select track']).callsArgWith(1, null, { 'Select track': 123 });
+    this.mockPrompt.expects('get').once().withArgs(['Select track']).callsArgWith(1, null, { 'Select track': 'e' });
+    this.mockProcess.expects('exit').once().withExactArgs(0);
+    this.stub(Roombox.prototype, 'play', function (answer, opts, cb) {
+      cb();
+    });
+    this.stub(Roombox.prototype, 'start', function (opts, cb) {
+      assert.equals(opts, { baudrate: 57600, dir: "data/", path: '/dev/tty' });
+      cb();
+    });
+    cli.exec();
+  }
+});
---
+++ test/roombox.js
@@ -0,0 +1,95 @@
+var buster = require('buster-node'),
+  referee = require('referee'),
+  Playlist = require('../lib/playlist'),
+  roomba = require('roomba'),
+  Roombox = require('../lib/roombox'),
+  assert = referee.assert;
+
+buster.testCase('roombox - start', {
+  setUp: function () {
+    this.mockConsole = this.mock(console);
+    this.mockRoomba = this.mock(roomba);
+
+    var mockBot = {
+      once: function (event, cb) {
+        assert.equals(event, 'ready');
+        cb();
+      }
+    };
+    this.mockRoomba.expects('Roomba').once().returns(mockBot);
+
+    this.stub(Playlist.prototype, 'load', function (dir) {
+      assert.equals(dir, 'somedir');
+    });
+
+    this.roombox = new Roombox();
+  },
+  'should ready roomba and display songs on playlist': function (done) {
+    this.mockConsole.expects('log').once().withExactArgs('Loading songs on %s', 'somedir');
+    this.mockConsole.expects('log').once().withExactArgs('Starting Roombox with path %s at baudrate %d', '/dev/tty.roomba', 57600);
+
+    var displaySongsIsCalled = false;
+    this.stub(Playlist.prototype, 'displaySongs', function () {
+      displaySongsIsCalled = true;
+    });
+
+    this.roombox.start({ dir: 'somedir' }, function () {
+      assert.equals(displaySongsIsCalled, true);
+      done();
+    });
+  },
+  'should log custom opts': function (done) {
+    this.mockConsole.expects('log').once().withExactArgs('Loading songs on %s', 'somedir');
+    this.mockConsole.expects('log').once().withExactArgs('Starting Roombox with path %s at baudrate %d', '/somepath', 30000);
+
+    var displaySongsIsCalled = false;
+    this.stub(Playlist.prototype, 'displaySongs', function () {
+      displaySongsIsCalled = true;
+    });
+
+    this.roombox.start({ dir: 'somedir', path: '/somepath', baudrate: 30000 }, function () {
+      assert.equals(displaySongsIsCalled, true);
+      done();
+    });
+  }
+});
+
+buster.testCase('roombox - play', {
+  setUp: function () {
+    this.mockConsole = this.mock(console);
+
+    this.stub(Playlist.prototype, 'getSong', function (trackNumber) {
+      assert.equals(trackNumber, 888);
+      return {
+        title: 'some title',
+        by: 'some singer',
+        notes: [
+          [67, 64], [67, 64], [67, 64], [67, 64],
+          [67, 64], [67, 64], [67, 64], [67, 64],
+          [67, 64], [67, 64], [67, 64], [67, 64],
+          [67, 64], [67, 64], [67, 64], [67, 64]
+        ]
+      };
+    });
+
+    this.roombox = new Roombox();
+  },
+  'should convert song into segments and send them to bot': function (done) {
+    this.mockConsole.expects('log').once().withExactArgs('Preparing track %d: %s - %s', 888, 'some title', 'some singer');
+    this.mockConsole.expects('log').once().withExactArgs('Playing segment %d with duration %d ms', 0, 9500);
+
+    var self = this;
+    this.botData = [];
+    this.roombox.bot = {
+      send: function (data) {
+        self.botData.push(data);
+      }
+    };
+
+    this.roombox.play(888, { interval: 1 }, function () {
+      assert.equals(self.botData[0].cmd, 'SONG');
+      assert.equals(self.botData[1].cmd, 'PLAY');
+      done();
+    });
+  }
+});
